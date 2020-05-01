e3c7885f0aa39e79abfb01982bacc2ce3f73dbae
Gilles Frappier <gilles.frappier@outlook.com>
2020-05-01 11:27:16 -0400

    Changes

--- jenkins/scripts/run-certifiers.sh
+++ jenkins/scripts/run-certifiers.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
 echo 'Running certifiers on code'
+set -x
+sleep 1
+echo $! > .pidfile
+set +x
--- jenkins/scripts/run-snyk.sh
+++ jenkins/scripts/run-snyk.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
 echo 'Running Snyk on code'
+set -x
+sleep 1
+echo $! > .pidfile
+set +x
