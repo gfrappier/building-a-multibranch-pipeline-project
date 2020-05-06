0f42df0f495993140a9932075ee70362fd8e86ce
Gilles Frappier <gilles.frappier@outlook.com>
2020-05-06 15:21:28 -0400

    Added @Field

--- jenkins/scripts/Constants.groovy
+++ jenkins/scripts/Constants.groovy
@@ -1,3 +1,8 @@
+import groovy.transform.Field
+
+@Field
+def SingleMessage="This is the content of the single message"
+
 def Messages = [Msg1: 'Test message 1', Msg2: 'Test message 2', Msg3: 'Test message 3']
-def SingleMessage = "This is the content of the single message"
+
 return this;
